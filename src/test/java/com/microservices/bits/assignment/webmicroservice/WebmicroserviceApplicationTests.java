85a9e467b30956ab781a9c198e942a36c623ab89
bits-shivraj <2022mt03564@wilp.bits-pilani.ac.in>
2024-03-10 11:30:14 +0530

    Controller added and spotbugs added in plugin in pom.xml

--- src/test/java/com/microservices/bits/assignment/webmicroservice/WebmicroserviceApplicationTests.java
+++ src/test/java/com/microservices/bits/assignment/webmicroservice/WebmicroserviceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.microservices.bits.assignment.webmicroservice;
 
+import com.microservices.bits.assignment.webmicroservice.Controller.webController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class WebmicroserviceApplicationTests {
+public class WebmicroserviceApplicationTests {
+    @Autowired
+    private webController testController;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testPostTestAPI() {
+        // Mock the HTTP request
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setRequestURI("/test");
 
+        // Mock the HTTP response
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // Call the controller method
+        String res = testController.test();
+
+        // Verify the response
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("webservice11",res);
+    }
+
+    @Test
+    void testPostdoRegisterAPI() throws IOException, InterruptedException {
+        // Mock the HTTP request
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setRequestURI("/doRegister");
+
+        HashMap<String, String> body = new HashMap<>();
+        body.put("mobileno", "12345678900");
+        body.put("empname","shiv");
+        body.put("empid","123");
+        body.put("emptype", "34");
+        body.put("secretkey", "1234567890");
+        // Mock the HTTP response
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // Call the controller method
+      ResponseEntity<?>  res =  testController.doRegister(body);
+
+        // Verify the response
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+       // assertEquals("webservice",res.getBody());
+    }
 }
---
+++ src/test/java/com/microservices/bits/assignment/webmicroservice/WebmicroserviceApplicationTests1.java
@@ -0,0 +1,13 @@
+package com.microservices.bits.assignment.webmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmicroserviceApplicationTests1 {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
